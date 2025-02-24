1451f459b6b210a90a31c383b87addf84285b9d3
YurleyBG <yurleybgarcia20@gmail.com>
2025-02-24 17:43:34 -0500

    feat :sparkles: finish

--- Dia6/src/dia6/InventarioTest.java
+++ Dia6/src/dia6/InventarioTest.java
@@ -1,62 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dia6;
 
 import java.util.*;
 public class InventarioTest {
     
-     public static void main(String[] args) {
-         Scanner scan=new Scanner(System.in); 
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in); 
        
         Producto GI1=new Producto();
-        Producto GI2=new Producto("ca1456","cafe",10,5000);
-        GI1.setNombre("Arroz");
-        GI1.setCodigo("1236ea");
-        GI1.setCantidad(30);
-        GI1.setPrecio(4000);
-        System.out.println("Nombre: "+GI1.getNombre());
-        System.out.println("Codigo: " + GI1.getCodigo());
-        System.out.println("Cantidad: "+GI1.getCantidad());
-        System.out.println("Precio: "+ GI1.getPrecio());
-        System.out.println("------------------------");
-        System.out.println("Nombre: "+GI2.getNombre());
-        System.out.println("Codigo: " + GI2.getCodigo());
-        System.out.println("Cantidad: "+GI2.getCantidad());
-        System.out.println("Precio: "+ GI2.getPrecio());
-        
-         System.out.println("");
-         System.out.println("----MENU-----");
-         System.out.println("1.comprar");
-         System.out.println("2.agregar");
-         System.out.println("");
-         System.out.println("ingrese una la opcion que desea");
-         int opc=scan.nextInt();
-         if(opc==1){
-            System.out.println("ingrese la cantida que va a comprar");
-            int can=scan.nextInt();
-            GI1.actualizarStoc(can);
-            System.out.println(GI1.getCantidad());
-         
-         }
-         else if(opc==2){
-             System.out.println("ingrese la cantida que va ingresar");
-            int can=scan.nextInt();
-            GI1.actualizarStoc(can);
-            System.out.println(GI1.getCantidad());
-         
+        ArrayList<Producto>producticos=new ArrayList<>();
+        producticos.add(new Producto("c3245A","Cafe",30,10000));
+        producticos.add(new Producto("c3345A","panela",20,8000));
+        producticos.add(new Producto("c3445A","Arroz",40,4000));
+        for (Producto i :producticos) {
+            System.out.println("");
+            System.out.println("Cosdigo: "+i.getCodigo());
+            System.out.println("Nombre: "+i.getNombre());
+            System.out.println("Cantidad: "+i.getCantidad());
+            System.out.println("Precio: "+i.getPrecio());
+           
+        }
+        System.out.println("");
+        System.out.println("----MENU-----");
+        System.out.println("1.comprar");
+        System.out.println("2.agregar");
+        System.out.println("");
+        System.out.println("ingrese una la opcion que desea");
+        int opc=scan.nextInt();
+        if(opc==1){
+            scan.nextLine();
+            System.out.println("ingrese el codigo del producto");
+            String Code=scan.nextLine();
+            for (Producto i :producticos) {
+                if(Code.equalsIgnoreCase(i.getCodigo())){
+                    
+                    System.out.println("ingrese la cantidad que va a comprar(en numeros negativos)");
+                    int can=scan.nextInt();
+                    i.actualizarStoc(can);
+                    System.out.println(i.getCantidad());
+                    
+                    System.out.println("----------------");
+                    System.out.println("Nombre: "+i.getNombre());
+                    System.out.println("Cantidad: "+i.getCantidad());
+                    System.out.println("Precio: "+i.getPrecio()*can*-1);
+                }
+            }    
+
+        }
+        else if(opc==2){
+           scan.nextLine();
+           System.out.println("ingrese el codigo del producto");
+           String Code=scan.nextLine();
+            for (Producto i :producticos) {
+                if(Code.equalsIgnoreCase(i.getCodigo())){
+                    System.out.println("ingrese la cantida que va ingresar");
+                    int can=scan.nextInt();
+                    i.actualizarStoc(can);
+                    System.out.println(i.getCantidad());
+                    
+                    System.out.println("----------------");
+                    System.out.println("Nombre: "+i.getNombre());
+                    System.out.println("Cantidad: "+i.getCantidad());
+                   
+                    
+                }
+            } 
+           
+
         }
         else{
              System.out.println("esta opcion es invalida.");
          
         }
-        
-        
-        
-        
-        
-     
+   
     }
     
 }
