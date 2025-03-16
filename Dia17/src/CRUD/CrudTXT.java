32e7574f7e90eb6837b8bc698203748d0630659e
YurleyBG <yurleybgarcia20@gmail.com>
2025-03-16 15:07:40 -0500

    feat :sparkles: finish

--- Dia17/src/CRUD/CrudTXT.java
+++ Dia17/src/CRUD/CrudTXT.java
@@ -3,6 +3,8 @@ package CRUD;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,7 +54,7 @@ public class CrudTXT {
     public static void ListarProductos(){
     
         if (productos.isEmpty()){
-            System.out.println("No se pudo listar la persona porque no hay");
+            System.out.println("No se pudo listar los productos porque no hay");
         }
         else{
             for (Producto producto : productos ) {
@@ -63,5 +65,72 @@ public class CrudTXT {
         }
     
     }
+    public static void guardarPersona() {
+        try (FileWriter writer = new FileWriter(archivo)) {
+            for (Persona p : personas) {
+                writer.write(p.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error al guardar la persona: " + e.getMessage());
+        }
+    }
     
+    public static void guardarProducto() {
+        try (FileWriter writer = new FileWriter(archivo)) {
+            for (Producto pro : productos) {
+                writer.write(pro.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error al guardar el producto: " + e.getMessage());
+        }
+    }
+    
+    
+    
+    public static void agregarPersona(int id, String nombre, int edad) {
+        personas.add(new Persona(id, nombre, edad));
+        guardarPersona();
+    }
+    
+    
+    public static void agregarProductos(int id, String nombre, double precio) {
+        productos.add(new Producto(id, nombre, precio));
+        guardarProducto();
+    }
+    
+    
+    public static void actualizarPersona(int id, String nuevoNombre, int nuevaEdad) {
+        for (Persona p : personas) {
+            if (p.getId() == id) {
+                p.setNombre(nuevoNombre);
+                p.setEdad(nuevaEdad);
+                guardarPersona();
+                return;
+            }
+        }
+        System.out.println("Persona con ID " + id + " no encontrada.");
+    }
+    public static void actualizarProducto(int id, String nuevoNombre, double nuevoPrecio) {
+        for (Producto pro : productos) {
+            if (pro.getId() == id) {
+                pro.setNombre(nuevoNombre);
+                pro.setPrecio(nuevoPrecio);
+                guardarPersona();
+                return;
+            }
+        }
+        System.out.println("Producto con ID " + id + " no encontrada.");
+    }
+    
+    
+    public static void eliminarPersona(int id) {
+        personas.removeIf(p -> p.getId() == id);
+        guardarPersona();
+    }
+    
+    
+    public static void eliminarProducto(int id) {
+        productos.removeIf(pro -> pro.getId() == id);
+        guardarProducto();
+    }
 }
--- Dia17/src/CRUD/main.java
+++ Dia17/src/CRUD/main.java
@@ -2,12 +2,190 @@
 package CRUD;
 
 import static CRUD.CrudTXT.*;
+import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
         cargarDatos();
-        ListarPersonas();
-        ListarProductos();
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        boolean bool =true;
+        while(bool){
+        
+            System.out.println("\n--- CRUD ---");
+            System.out.println("1. Agregar Persona/Producto");
+            System.out.println("2. Listar Persona/Producto");
+            System.out.println("3. Actualizar Persona/Producto");
+            System.out.println("4. Eliminar Persona/Producto");
+            System.out.println("5. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = sc.nextInt();
+  
+            
+            switch(opcion){
+                
+                case 1:
+                    System.out.println("--------AGREGAR-----------");
+                    System.out.println("");
+                    System.out.println("\n--- Opciones ---");
+                    System.out.println("1. Persona");
+                    System.out.println("2. Producto");
+                    System.out.println("3. Regresar");
+                    System.out.println("___________________");
+                    System.out.println("Elige una opcion: ");
+                    int opc = sc.nextInt();
+                    switch(opc){
+                        case 1:
+                            System.out.print("Ingrese la id: ");
+                            int id = sc.nextInt();
+                            sc.nextLine(); 
+                            System.out.print(" ingrese el Nombre: ");
+                            String nombre = sc.nextLine();
+                            System.out.print("ingrese la Edad: ");
+                            int edad = sc.nextInt();
+                            agregarPersona(id, nombre, edad);
+                            guardarPersona();
+                        break;
+                        case 2:
+                            System.out.print("Ingrese la id: ");
+                            int idP = sc.nextInt();
+                            sc.nextLine();
+                            System.out.print(" ingrese el Nombre: ");
+                            String nombreP = sc.nextLine();
+                            System.out.print("ingrese el precio: ");
+                            double precio = sc.nextDouble();
+                            agregarProductos(idP, nombreP, precio);
+                            guardarProducto();
+                        break;
+                        case 3:
+                            System.out.println("Regresando ....");
+                            bool=true;
+                        break;
+                        default:
+                            System.out.println("Opción no válida.");
+                    }
+                break;
+                case 2:
+                    System.out.println("-----------LISTAR----------");
+                    System.out.println("");
+                    System.out.println("\n--- Opciones ---");
+                    System.out.println("1. Persona");
+                    System.out.println("2. Producto");
+                    System.out.println("3. Regresar");
+                    System.out.println("___________________");
+                    System.out.println("Elige una opcion: ");
+                    int opc0 = sc.nextInt();
+                    switch(opc0){
+                        case 1:
+                            ListarPersonas();
+                        break;
+                        case 2:
+                            ListarProductos();
+                        break;
+                        case 3:
+                            System.out.println("Regresando ....");
+                            bool=true;
+                        break;
+                        default:
+                            System.out.println("Opción no válida.");
+                    
+                    }
+                break;
+                case 3:
+                    System.out.println("---------ACTUALIZAR-----------");
+                    System.out.println("");
+                    System.out.println("\n--- Opciones ---");
+                    System.out.println("1. Persona");
+                    System.out.println("2. Producto");
+                    System.out.println("3. Regresar");
+                    System.out.println("___________________");
+                    System.out.println("Elige una opcion: ");
+                    int opc1 = sc.nextInt();
+                    switch(opc1){
+                        case 1:
+                            System.out.print("ingrese el id de la persona a actualizar: ");
+                            int idActualizar = sc.nextInt();
+                            sc.nextLine();
+                            System.out.print("Nuevo nombre: ");
+                            String nuevoNombre = sc.nextLine();
+                            System.out.print("Nueva edad: ");
+                            int nuevaEdad = sc.nextInt();
+                            actualizarPersona(idActualizar, nuevoNombre, nuevaEdad);
+                            guardarPersona();
+                        break;
+                        case 2:
+                            System.out.print("ingrese el id de la persona a actualizar: ");
+                            int idActualizar1 = sc.nextInt();
+                            sc.nextLine();
+                            System.out.print(" ingrese el Nuevo nombre: ");
+                            String nuevoNombre1 = sc.nextLine();
+                            System.out.print("ingrese la Nueva edad: ");
+                            double precio = sc.nextDouble();
+                            actualizarProducto(idActualizar1, nuevoNombre1, precio);
+                            guardarProducto();
+                        break;
+                        case 3:
+                            System.out.println("Regresando ....");
+                            bool=true;
+                        break;
+                        default:
+                            System.out.println("Opción no válida.");
+                    
+                    
+                    
+                    }
+                break;
+                case 4:
+                    System.out.println("---------ELIMINAR-----------");
+                    System.out.println("");
+                    System.out.println("\n--- Opciones ---");
+                    System.out.println("1. Persona");
+                    System.out.println("2. Producto");
+                    System.out.println("3. Regresar");
+                    System.out.println("___________________");
+                    System.out.println("Elige una opcion: ");
+                    int opc2 = sc.nextInt();
+                    switch(opc2){
+                        case 1:
+                            System.out.print("ingrese la id de la persona a eliminar: ");
+                            int idEliminar = sc.nextInt();
+                            eliminarPersona(idEliminar);
+                            guardarPersona();
+                        break;
+                        case 2:
+                            System.out.print("ingrese la id de la persona a eliminar: ");
+                            int idEliminar1 = sc.nextInt();
+                            eliminarProducto(idEliminar1);
+                            guardarProducto();
+                        break;
+                        case 3:
+                            System.out.println("Regresando ....");
+                            bool=true;
+                        break;
+                        default:
+                            System.out.println("Opción no válida.");
+                    
+                    
+                    
+                    }
+                break;
+                case 5:
+                    System.out.println("Haz abandonado el programa :)");
+                    bool=false;
+                break;
+                default:
+                    System.out.println("Opción no válida.");
+           
+            
+            
+            }
+        
+        
+        
+        }
+        
     }
    
 }
